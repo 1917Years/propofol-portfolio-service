7c698cb4a31f398a08fc745cf8aedc5e069e0e7d
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-04-30 19:38:19 +0900

    :bug: Fix) @Transactional 추가 (개인 정의 메서드)

--- src/main/java/propofol/ptfservice/domain/portfolio/repository/CareerRepository.java
+++ src/main/java/propofol/ptfservice/domain/portfolio/repository/CareerRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 import propofol.ptfservice.domain.portfolio.entity.Career;
 
 public interface CareerRepository extends JpaRepository<Career, Long> {
+    @Transactional
     @Modifying
     @Query("delete from Career c where c.portfolio.id =:portfolioId")
     int bulkDeleteAll(@Param(value = "portfolioId") Long portfolioId);
