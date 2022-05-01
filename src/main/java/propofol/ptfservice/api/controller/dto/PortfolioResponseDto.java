d7bd49a121ea473981d24bf15cfd395d00023fca
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-02 01:21:57 +0900

    Fix) PortfolioDto -> PortfolioDetailResponseDto 변경 (api-domain 분리)

--- src/main/java/propofol/ptfservice/api/controller/dto/PortfolioResponseDto.java
+++ src/main/java/propofol/ptfservice/api/controller/dto/PortfolioResponseDto.java
@@ -2,19 +2,8 @@ package propofol.ptfservice.api.controller.dto;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import propofol.ptfservice.domain.portfolio.entity.Archive;
-import propofol.ptfservice.domain.portfolio.entity.Career;
-import propofol.ptfservice.domain.portfolio.entity.Project;
-import propofol.ptfservice.domain.portfolio.entity.Template;
-import propofol.ptfservice.domain.portfolio.service.dto.ArchiveDto;
-import propofol.ptfservice.domain.portfolio.service.dto.CareerDto;
-import propofol.ptfservice.domain.portfolio.service.dto.PortfolioDto;
-import propofol.ptfservice.domain.portfolio.service.dto.ProjectDto;
 
-import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -26,6 +15,6 @@ public class PortfolioResponseDto {
     private String degree;
     private String score;
 
-    private PortfolioDto portfolioDto;
+    private PortfolioDetailResponseDto portfolioDto;
 
 }
