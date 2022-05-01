46ed468ea54bd59ba3005d55cc74fdc6600e1d88
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-02 01:21:26 +0900

    Feat) PortfolioDto 생성자 추가

--- src/main/java/propofol/ptfservice/domain/portfolio/service/dto/PortfolioDto.java
+++ src/main/java/propofol/ptfservice/domain/portfolio/service/dto/PortfolioDto.java
@@ -4,16 +4,21 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import propofol.ptfservice.domain.portfolio.entity.Template;
 
-import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 public class PortfolioDto {
-    @NotEmpty
     private Template template;
     private List<ArchiveDto> archives = new ArrayList<>();
     private List<CareerDto> careers = new ArrayList<>();
     private List<ProjectDto> projects = new ArrayList<>();
+
+    public PortfolioDto(Template template, List<ArchiveDto> archives, List<CareerDto> careers, List<ProjectDto> projects) {
+        this.template = template;
+        this.archives = archives;
+        this.careers = careers;
+        this.projects = projects;
+    }
 }
