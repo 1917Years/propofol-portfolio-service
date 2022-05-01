fe4903dc86ad1c49a9e9aa01943c456f629cea97
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-02 00:55:25 +0900

    Fix) CareerDto, ProjectDto, CareerDto 제약 조건 제거

--- src/main/java/propofol/ptfservice/domain/portfolio/service/dto/ArchiveDto.java
+++ src/main/java/propofol/ptfservice/domain/portfolio/service/dto/ArchiveDto.java
@@ -3,13 +3,9 @@ package propofol.ptfservice.domain.portfolio.service.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Pattern;
-
 @Data
 @NoArgsConstructor
 public class ArchiveDto {
-    @Pattern(regexp="^((http(s?))\\:\\/\\/)([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]{2,6}(\\:[0-9]+)?(\\/\\S*)?$",
-            message = "Link 형식이 유효하지 않습니다.")
     private String link;
     private String content;
 }
--- src/main/java/propofol/ptfservice/domain/portfolio/service/dto/CareerDto.java
+++ src/main/java/propofol/ptfservice/domain/portfolio/service/dto/CareerDto.java
@@ -3,16 +3,11 @@ package propofol.ptfservice.domain.portfolio.service.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Pattern;
-import java.time.LocalDate;
-
 @Data
 @NoArgsConstructor
 public class CareerDto {
     private String title;
-    @Pattern(regexp = "^[0-9]{4}-[0-9]{2}", message = "올바른 날짜 형식이 아닙니다.")
     private String startTerm;
-    @Pattern(regexp = "^[0-9]{4}-[0-9]{2}", message = "올바른 날짜 형식이 아닙니다.")
     private String endTerm;
     private String basicContent;
     private String detailContent;
