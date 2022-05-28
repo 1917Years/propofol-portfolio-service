25905dd2773caf80b3f9af209a5cef351bfe0053
Cl8D <46810837+Cl8D@users.noreply.github.com>
2022-05-28 17:27:32 +0900

    fix) basicInfo 수정

--- src/main/java/propofol/ptfservice/api/controller/dto/BasicInfoCreateRequestDto.java
+++ src/main/java/propofol/ptfservice/api/controller/dto/BasicInfoCreateRequestDto.java
@@ -8,8 +8,6 @@ import javax.validation.constraints.Pattern;
 @Data
 @NoArgsConstructor
 public class BasicInfoCreateRequestDto {
-    @Pattern(regexp="^((http(s?))\\:\\/\\/)([0-9a-zA-Z\\-]+\\.)+[a-zA-Z]{2,6}(\\:[0-9]+)?(\\/\\S*)?$",
-            message = "Link 형식이 유효하지 않습니다.")
     private String github;
     private String job;
     private String content;
